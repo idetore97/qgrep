43c769c764e753b7e270a693069e42ee0b72645e
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2022-05-05 01:09:28 -0400

    QGREP functionality

--- qgrep.py
+++ qgrep.py
@@ -3,6 +3,7 @@ Python code to do string-search using Grover's algorithm.
 """
 import grover_pattern as gp
 import str_utils as su
+import numpy as np
 import argparse
 
 # Get arguments
@@ -12,27 +13,49 @@ parser.add_argument('target', help='String to search for.') # Get string to sear
 args = parser.parse_args()
 
 # Get file data
-file = open(args.filename)
+file = args.filename
 
 # Convert files to array of strings based on line numbers
 lines = su.get_lines_from_file(file)
 
-# Convert file to array of ints
-encoding = su.strings2ints(lines)
+# Convert file and target to ints
 target = su.encode_str(args.target)
+encoding = su.strings2ints(lines)
 
-# Create oracle
-num_qubits = 10
-integer_to_find = target
-num_iter = 20
-qc = gp.grover_circuit(num_qubits, integer_to_find, num_iter)
-gp.bypass_draw(qc, bypass=True)
+# Get inputs for oracle creation
+num_qubits = np.ceil(np.log2(encoding.max() + 1)).astype(int)
+num_iter = 12
+master_indices = []
 
-# Simulate results
-gp.simulate_results(qc, show_hist=False)
+for i, target_int in enumerate(target):
+    # Create oracle
+    qc = gp.grover_circuit(num_qubits, target_int, num_iter)
+
+    # Simulate results
+    probabilities = gp.match_ints(encoding.flatten(), qc)
+
+    # Get indices of candidates
+    indices = np.argwhere(probabilities >= np.mean(probabilities))
+
+    # Compare indices and see if sequential -- potential solutions
+    indices_to_remove = []
+    if i == 0:
+        master_indices = indices
+    else:
+        for j in master_indices:
+            if j+i not in indices:
+                indices_to_remove.append(j)
+    
+    # Remove all failed comparisons
+    for j in indices_to_remove:
+        master_indices.remove(j)
 
 # Turn simulated data into user-legible output
+line_numbers = np.floor(master_indices / encoding.shape[1])
 
+# Print all lines to user
+for i in line_numbers:
+    print(lines[i])
 
 
 
